05872ade2e27602488daf1a82feb906516d50449
wanshariff-fig8 <syazwanshariff@gmail.com>
2025-09-04 19:11:41 +0800

    Remove problematic ms directory causing build failures

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -106,7 +106,7 @@ function MobileMenu({
 
       {/* Mobile Menu Panel */}
       <div
-        className={`fixed top-0 right-0 h-full w-80 bg-olive-dark border-l border-accent/20 z-50 transform transition-transform duration-300 lg:hidden ${
+        className={`fixed top-0 right-0 h-screen w-80 bg-olive-dark border-l border-accent/20 z-50 transform transition-transform duration-300 lg:hidden ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
